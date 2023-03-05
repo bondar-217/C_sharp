b7353b0374c80aa984f24a4c923b7bd2c7ecb8a1
Anatoliy Bondarenko <anatoliy.bondarenko.03.1988@gmail.com>
2023-03-05 22:30:54 +0300

    сделал задания ко 2 семинару

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,35 +1,7 @@
-﻿/*Console.Write("Input your number: ");
-int a = Convert.ToInt32(Console.ReadLine());
-
-bool Kratnost(int num)
-{
-    if (num % 7 == 0 && num % 23 == 0) return true;
-    else return false;
-}
-
-bool result = Kratnost(a);
-Console.WriteLine(result);
-*/
-
+﻿
 /*
-Console.Write("Input your number 1: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input your number 2: ");
-int b = Convert.ToInt32(Console.ReadLine());
-
-
-bool Kvadrat(int num1, int num2)
-{
-    if ((num2 == num1 * num1) || (num1 == num2 * num2)) return true;
-    else return false;
-}
-
-bool result = Kvadrat(a, b);
-Console.WriteLine(result);
-*/
-
 //Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-/*int SecondValue(int num)
+int SecondValue(int num)
 {
     int second = num % 100 / 10;
     return second;
@@ -40,22 +12,47 @@ Console.WriteLine(randomnum);
 Console.WriteLine(SecondValue(randomnum));
 */
 
-
+/*
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
-int LastValue(int num)
+int LastValue(int number)
 {
+    int result = -1;
+    if (number >= 100)
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        result = number % 10;
+    }
+    return result;
 
-    //int last = (num % 10);
-    int last = 0;
-    if (last < 100) Console.Write("Нет третьего числа: ");
-    else return last = (num % 10);
-    //if (last < 100) Console.Write("Нет третьего числа: ");
 }
-
-
 Console.Write("Input your number : ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(number);
+if (LastValue(number) == -1) Console.WriteLine("третьей цифры нет");
+else Console.WriteLine(number);
 Console.WriteLine(LastValue(number));
+*/
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.Write("Введи день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+void Weekendornot(int day)
+{
+    if (day == 6 || day == 7)
+    {
+        Console.WriteLine("выходной)");
+    }
+    else if (day > 7 | day < 0) Console.WriteLine("ошибка");
+    else Console.WriteLine("(арбайтен)");
+
+}
+Weekendornot(day);
